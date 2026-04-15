169b1436be408099c5c06cef40d5a884f489a926
Eduardo S. Nascimento <edusan3456@gmail.com>
2026-04-15 20:27:36 -0300

    chore: infraestrutura base do projeto

---
+++ src/main/java/com/diarioclasse/exception/AcessoNegadoException.java
@@ -0,0 +1,7 @@
+﻿package com.diarioclasse.exception;
+
+public class AcessoNegadoException extends RuntimeException {
+    public AcessoNegadoException( String mensagem) {
+        super(mensagem);
+    }
+}
---
+++ src/main/java/com/diarioclasse/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+﻿package com.diarioclasse.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    public RecursoNaoEncontradoException( String mensagem) {
+        super(mensagem);
+    }
+}
